def627c8dc65d5ab364301c997b454eb7559c94f
gabrielav2004 <145474456+gabrielav2004@users.noreply.github.com>
2025-03-19 15:48:03 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -21,7 +21,7 @@ export default function Home() {
             .
           </li>
           <li className="tracking-[-.01em]">
-            Save and see your changes instantly. new change was done...
+            Save and see your changes instantly. new change was done... Another change
           </li>
         </ol>
 
