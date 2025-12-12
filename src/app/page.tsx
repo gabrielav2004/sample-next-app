a930ab3e90b12537983bc4e1bb0b063c436a14fa
gabrielav2004 <145474456+gabrielav2004@users.noreply.github.com>
2025-12-12 10:37:26 +0530

    Grafana OTEL Loki Test - 1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -21,7 +21,7 @@ export default function Home() {
             .
           </li>
           <li className="tracking-[-.01em]">
-            Save and see your changes instantly. Grafana Loki Alloy Test - 8
+            Save and see your changes instantly. Grafana Loki Alloy Test - 9
           </li>
         </ol>
 
